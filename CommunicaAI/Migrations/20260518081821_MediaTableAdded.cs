d72aa7c919faa89f4f15d1af8e370f83d5f611f7
vignesh gazula <gazula.2006@gmail.com>
2026-05-18 13:48:57 +0530

    Added Media Table Migrations

---
+++ CommunicaAI/Migrations/20260518081821_MediaTableAdded.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CommunicaAI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MediaTableAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppUsers",
+                table: "AppUsers");
+
+            migrationBuilder.RenameTable(
+                name: "AppUsers",
+                newName: "Users");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AppUsers_Email",
+                table: "Users",
+                newName: "IX_Users_Email");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "UserMediaProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AudioUrl = table.Column<string>(type: "text", nullable: true),
+                    AudioPublicId = table.Column<string>(type: "text", nullable: true),
+                    AudioContentType = table.Column<string>(type: "text", nullable: true),
+                    AudioSizeBytes = table.Column<long>(type: "bigint", nullable: true),
+                    AudioUploadedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    VideoUrl = table.Column<string>(type: "text", nullable: true),
+                    VideoPublicId = table.Column<string>(type: "text", nullable: true),
+                    VideoContentType = table.Column<string>(type: "text", nullable: true),
+                    VideoSizeBytes = table.Column<long>(type: "bigint", nullable: true),
+                    VideoUploadedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMediaProfiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserMediaProfiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserMediaProfiles_UserId",
+                table: "UserMediaProfiles",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserMediaProfiles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "AppUsers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_Email",
+                table: "AppUsers",
+                newName: "IX_AppUsers_Email");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppUsers",
+                table: "AppUsers",
+                column: "Id");
+        }
+    }
+}
